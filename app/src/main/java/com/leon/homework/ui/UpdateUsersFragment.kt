211507d01d4a4a36810c317409d031983a151381
LeonChien <st20031213taiwan@gmail.com>
2022-05-29 22:35:01 +0800

    [Add] Back to ListInfoFragment.

--- app/src/main/java/com/leon/homework/ui/UpdateUsersFragment.kt
+++ app/src/main/java/com/leon/homework/ui/UpdateUsersFragment.kt
@@ -5,6 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
+import com.leon.homework.R
 import com.leon.homework.databinding.FragmentUpdateUsersBinding
 
 class UpdateUsersFragment : Fragment() {
@@ -15,6 +18,11 @@ class UpdateUsersFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val binding = FragmentUpdateUsersBinding.inflate(layoutInflater, container, false)
+
+        binding.btnBack.setOnClickListener {
+            Navigation.findNavController(binding.root)
+                .navigate(R.id.action_update_users_to_list_info)
+        }
         return binding.root
     }
 }
